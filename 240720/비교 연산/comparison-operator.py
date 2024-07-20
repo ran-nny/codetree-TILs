84326f5601f872d9316b76180204c05bc872ee6a
란님 <113058078+ran-nny@users.noreply.github.com>
2024-07-20 16:37:15 +0900

    Title: 비교 연산, Time: 209ms, Memory: 24MB, Status: Passed - CodeTree

--- 240720/비교 연산/comparison-operator.py
+++ 240720/비교 연산/comparison-operator.py
@@ -3,32 +3,9 @@ n=input().split()
 a=int(n[0])
 b=int(n[1])
 
-if a>=b:
-    print(1)
-else:
-    print(0)
-
-if a>b:
-    print(1)
-else:
-    print(0)
-
-if b>=a:
-    print(1)
-else:
-    print(0)
-
-if b>a:
-    print(1)
-else:
-    print(0)
-
-if a==b:
-    print(1)
-else:
-    print(0)
-
-if a!=b:
-    print(1)
-else:
-    print(0)
+print(int(a>=b))
+print(int(a>b))
+print(int(a<=b))
+print(int(a<b))
+print(int(a==b))
+print(int(a!=b))
