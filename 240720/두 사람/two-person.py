1ee8abf9405bfd6db6372419dd5618df3d8d803b
란님 <113058078+ran-nny@users.noreply.github.com>
2024-07-20 23:56:33 +0900

    Title: 두 사람, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240720/두 사람/two-person.py
@@ -0,0 +1,13 @@
+a = input().split()
+b= input().split()
+
+a_age = int(a[0])
+a_sex = a[1]
+
+b_age = int(b[0])
+b_sex = b[1]
+
+if (a_age >=19 and a_sex =='M') or (b_age >=19 and b_sex =='M'):
+    print(1)
+else:
+    print(0)
