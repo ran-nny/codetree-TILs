105097fce0cec4e1c8a5a7599b8af2df61b3915f
란님 <113058078+ran-nny@users.noreply.github.com>
2024-07-22 14:27:32 +0900

    Title: 최댓값 구하기, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240722/최댓값 구하기/maximum-value.py
+++ 240722/최댓값 구하기/maximum-value.py
@@ -14,3 +14,13 @@ elif b>a:
         print(b)
     else:
         print(c)
+elif c>a:
+    if c>b:
+        print(c)
+    else:
+        print(b)
+elif c>b:
+    if c>a:
+        print(c)
+    else:
+        print(a)
