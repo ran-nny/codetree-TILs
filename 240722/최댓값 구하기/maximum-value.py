de4d37c1c8ed6349b880500fed812aad08735720
란님 <113058078+ran-nny@users.noreply.github.com>
2024-07-22 14:32:48 +0900

    Title: 최댓값 구하기, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/최댓값 구하기/maximum-value.py
+++ 240722/최댓값 구하기/maximum-value.py
@@ -5,22 +5,19 @@ b=int(n[1])
 c=int(n[2])
 
 if a>b:
-    if a>c:
-        print(a)
-    else:
-        print(c)
-elif b>a:
-    if b>c:
-        print(b)
-    else:
-        print(c)
-elif c>a:
-    if c>b:
-        print(c)
-    else:
-        print(b)
-elif c>b:
     if c>a:
         print(c)
     else:
         print(a)
+
+elif b>c:
+    if a>b:
+        print(a)
+    else:
+        print(b)
+
+else:
+    if b>c:
+        print(b)
+    else:
+        print(c)
