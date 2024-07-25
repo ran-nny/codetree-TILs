f6cec8e17c3c47265f1cd498b2a4bb69ced5b175
란님 <113058078+ran-nny@users.noreply.github.com>
2024-07-25 12:07:37 +0900

    Title: 코로나 메뉴얼, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240725/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,30 @@
+p1 = input().split()
+p2 = input().split()
+p3 = input().split()
+
+s1 = p1[0]
+t1 = int(p1[1])
+
+s2 = p2[0]
+t2 = int(p2[1])
+
+s3 = p3[0]
+t3 = int(p3[1])
+
+if s1 == 'Y' and s2 == 'Y':
+    if t1 >= 37 and t2>=37:
+        print('E')
+    else:
+        print('N')
+elif s1 == 'Y' and s3 =='Y':
+    if t1 >= 37 and t3 >= 37:
+        print('E')
+    else:
+        print('N')
+elif s2 == 'Y' and s3 == 'Y':
+    if t2 >= 37 and t3 >= 37:
+        print('E')
+    else:
+        print('N')
+else:
+    print('N')
