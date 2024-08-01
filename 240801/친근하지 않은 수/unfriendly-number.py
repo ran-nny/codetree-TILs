5dab6c19cfc0d77e07ca4fc2275441f2098ca1d5
란님 <113058078+ran-nny@users.noreply.github.com>
2024-08-01 23:25:39 +0900

    Title: 친근하지 않은 수, Time: 217ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/친근하지 않은 수/unfriendly-number.py
+++ 240801/친근하지 않은 수/unfriendly-number.py
@@ -2,8 +2,8 @@ n =int(input())
 
 cnt = 0
 for i in range(1, n+1):
-    if i%2!=0:
-        if i%3!=0 and i%5!=0:
-            cnt+=1
-            
+    if i%2==0 or i%3==0 or i%5==0:
+        continue
+    else:
+        cnt+=1
 print(cnt)
