a345cd81930f6b601cf5f07c0747768f2fc1ef7e
란님 <113058078+ran-nny@users.noreply.github.com>
2024-08-02 14:58:03 +0900

    Title: 1까지 나누기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240802/1까지 나누기/divide-by-1.py
+++ 240802/1까지 나누기/divide-by-1.py
@@ -2,10 +2,10 @@ n = int(input())
 
 cnt = 0
 div = n
-for i in range(1, n+1):
-    div = div//i
-    cnt+=1
-    print(div)
+
+for i in range(1, 500):
+    div//=i
+    cnt +=1
     if div>=1:
         continue
     else:
