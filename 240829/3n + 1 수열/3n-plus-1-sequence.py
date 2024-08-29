5df6442050c5a4d0e9efc0c97814d8ba96e743a2
란님 <113058078+ran-nny@users.noreply.github.com>
2024-08-29 15:10:11 +0900

    Title: 3n + 1 수열, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240829/3n + 1 수열/3n-plus-1-sequence.py
+++ 240829/3n + 1 수열/3n-plus-1-sequence.py
@@ -2,13 +2,18 @@ n = int(input())
 cnt = 0
 
 while True:
+    # 무한루프 탈출 조건을 먼저 써주면 좋다. 
+    if n == 1:
+        break
+    
     if n % 2 == 0:
+        # 파이썬은 나누기 시 float으로 출력된다. 
         n = n//2
     else:
-        if n != 1:
-            n = n*3 +1
-        else:
-            break
+        n = 3*n + 1
+    
     cnt += 1
+
+
 
 print(cnt)
