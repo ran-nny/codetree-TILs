116ca9c49652de7562d8252ceda08fd1a404d0da
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-01 02:40:47 +0900

    Title: 별표 출력하기 8, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240901/별표 출력하기 8/print-star-8.py
+++ 240901/별표 출력하기 8/print-star-8.py
@@ -5,6 +5,6 @@ for i in range(n):
     if i % 2 == 0:
         print('*', end=' ')
     else:
-        for _ in range(2*i):
+        for _ in range(i+1):
             print('*', end=' ')
     print()
