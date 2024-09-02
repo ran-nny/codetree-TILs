3bb49ec81da238f2d2bca9639256c13b6c42bfcb
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-03 03:14:54 +0900

    Title: 구구단 만들기 2, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/구구단 만들기 2/print-multiplication-table-2.py
+++ 240903/구구단 만들기 2/print-multiplication-table-2.py
@@ -3,7 +3,7 @@ n = input().split()
 a = int(n[0])
 b = int(n[1])
 
-for i in range(1, 5):
+for i in range(1, 5): # 2, 9, 2 이렇게 하는 방법도 있다!
     for j in range(b, a-1, -1):
         print(f'{j} * {i*2} = {j*i*2}', end=' ')
 
