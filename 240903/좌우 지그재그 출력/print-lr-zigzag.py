7fcd4a91b7d1ac1e1ab8085ba57c1b8a64e64a4d
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-03 01:38:56 +0900

    Title: 좌우 지그재그 출력, Time: 136ms, Memory: 26MB, Status: Passed - CodeTree

--- 240903/좌우 지그재그 출력/print-lr-zigzag.py
+++ 240903/좌우 지그재그 출력/print-lr-zigzag.py
@@ -1,13 +1,13 @@
 n = int(input())
-cnt = 0
-
+	
 for i in range(n):
-    for j in range(n):
-        if i % 2 == 0:
-            cnt += 1
-            print(cnt, end=' ')
-        else:
-            print(cnt+n-j, end=' ')
-            if j == n-1:
-                cnt = cnt+n-j+(n-1)
-    print()
+	for j in range(n):
+    	# 짝수번째줄은 j를 더해 루프를 돌 때마다 1씩 증가
+        # 숫자가 i*n보다 커야하므로, 1을 더해준다
+		if i % 2 == 0:
+			print((i * n) + j + 1, end=" ")
+        # 홀수번째줄은 j를 빼 루프를 돌 때마다 1씩 감소
+        # 가장 큰 숫자인 (i+1)*n에 j를 뺀다
+		else:
+			print((i * n) + n - j, end=" ")
+	print()
