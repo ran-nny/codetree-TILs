25d03701e99b5aa51a5a703b2c03193b0d228281
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-03 03:10:41 +0900

    Title: 구구단 만들기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/구구단 만들기/print-multiplication-table.py
+++ 240903/구구단 만들기/print-multiplication-table.py
@@ -2,20 +2,11 @@ n = input().split()
 
 a = int(n[0]) #2
 b = int(n[1]) #6
-
-# 2이상 6이하의 수 중 짝수에 해당하는 숫자의 구구단, 
-# b에서 a로 감소하며 출력
-
-# 짝수의 개수 - j의 개수
-cnt = b//2 - a//2 + 1
-
+	
 for i in range(1, 10):
-    for j in range(1, cnt+1):
-        print(f"{b} * {i} = {b*i}", end=' ')
-        if j < cnt:
-            print('/', end=' ')
-        
-        b -=2
-        if j == cnt:
-            b += 2*cnt #b 원상복귀시키기
-    print()
+	for j in range(b, a-1, -1): #b부터 a까지 1씩 감소하면서
+		if j % 2 == 0:
+			print(f"{j} * {i} = {i * j}", end="")
+			if j != a:
+				print(" / ", end="");
+	print()
