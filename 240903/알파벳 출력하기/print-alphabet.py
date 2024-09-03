4a64f4a463ea292a39511f8f0df182f474b4b175
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-03 15:43:48 +0900

    Title: 알파벳 출력하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/알파벳 출력하기/print-alphabet.py
+++ 240903/알파벳 출력하기/print-alphabet.py
@@ -5,6 +5,6 @@ for i in range(n):
     for j in range(i+1):
         print(cnt, end='')
         cnt = chr(ord(cnt)+1)
-        if cnt == chr(ord('Z')+1):
+        if cnt == chr(ord('Z')+1): #ord(cnt) > ord('Z')로도 표현가능
             cnt ='A'
     print()
