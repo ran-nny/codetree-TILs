3f8638b9096716b391cf80377a2c644e90f42fef
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-06 13:36:21 +0900

    Title: 3n + 1 수열 m번, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240906/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
+++ 240906/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -6,15 +6,15 @@ for _ in range(m):
     cnt = 0
     if n == 1:
         print(cnt)
-        break
-    
-    while True: # 1이 될때까지 무한루프
-        if n % 2 == 0:
-            n //= 2
-        else:
-            n = 3 * n + 1
-        cnt += 1
-        if n == 1: #1이 되면 탈출
-            break
         
-    print(cnt)
+    else:
+        while True: # 1이 될때까지 무한루프
+            if n % 2 == 0:
+                n //= 2
+            else:
+                n = 3 * n + 1
+            cnt += 1
+            if n == 1: #1이 되면 탈출
+                break
+        
+        print(cnt)
