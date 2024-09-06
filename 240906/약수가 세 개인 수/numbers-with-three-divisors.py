beb3db747c4eb92ad1bcb5e431f4fabffadbb7f5
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-06 14:56:44 +0900

    Title: 약수가 세 개인 수, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240906/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 240906/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -2,7 +2,7 @@ n = input().split()
 
 start = int(n[0])
 end = int(n[1])
-cnt = 0
+ans = 0
 
 for curr_num in range(start, end+1):
     divisor_cnt = 0
@@ -10,6 +10,6 @@ for curr_num in range(start, end+1):
         if curr_num % divisor == 0:
             divisor_cnt += 1
     if divisor_cnt == 3:
-        cnt += 1
+        ans += 1
 
-print(cnt)
+print(ans)
