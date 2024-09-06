8e86f554001a042e81e0d53c203b8d29704b7484
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-06 15:24:18 +0900

    Title: 짝수의 합 n번 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240906/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.py
+++ 240906/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.py
@@ -1,9 +1,8 @@
 n = int(input())
 
 for i in range(n):
-    z = input().split()
-    a = int(z[0])
-    b = int(z[1])
+    inp = input().split()
+    a, b = int(z[0]), int(z[1])
     sum_val = 0
 
     for curr_num in range(a, b+1):
