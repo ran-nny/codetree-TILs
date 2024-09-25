f996d19f960f59fdd8e1fc455a932577600886e1
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-25 15:53:58 +0900

    Title: 학점 계산기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240925/학점 계산기/credit-calculator.py
+++ 240925/학점 계산기/credit-calculator.py
@@ -3,13 +3,10 @@ n = int(input())
 
 arr = list(map(float, input().split()))
 
-sum_val = 0
-for i in range(n):
-    sum_val += arr[i]
+sum_val = sum(arr)
+avg = sum_val/n
 
-avg = float(f"{sum_val/n:.1f}")
-
-print(avg)
+print(f"{avg:1f}")
 if avg >= 4.0:
     print('Perfect')
 elif avg >= 3.0:
