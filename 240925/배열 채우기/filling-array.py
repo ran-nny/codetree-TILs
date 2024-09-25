655ff189b1259d3422021754640b1fd454e98f7d
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-25 16:35:47 +0900

    Title: 배열 채우기, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240925/배열 채우기/filling-array.py
+++ 240925/배열 채우기/filling-array.py
@@ -1,13 +1,8 @@
 arr = list(map(int, input().split()))
-n = len(arr)    # 제일 끝 인덱스는 n-1
-idx = 0
 
-# 몇번째 원소에 0이 있는지 알아내기
-for i in range(n):  #   n까지 돌리기 0~n-1
+for i in range(len(arr)): 
     if arr[i] ==0:
-        idx = i-1 # 0나오기 전 인덱스까지
-    else:
-        idx = i
-
-for i in range(idx, -1, -1):
-    print(arr[i], end=' ')
+        for j in range(i-1, -1, -1):
+            print(arr[j], end=' ')
+        
+        break
