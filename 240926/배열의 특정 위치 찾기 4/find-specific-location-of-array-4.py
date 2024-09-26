b206974dcefe36b2b4375a0a435432d778632d80
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-26 15:14:59 +0900

    Title: 배열의 특정 위치 찾기 4, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240926/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
+++ 240926/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
@@ -1,16 +1,13 @@
 arr = list(map(int, input().split()))
 
 cnt = 0
-cnt_2 = 0
 sum_val = 0
+
 for elem in arr:
     if elem == 0:
         break
-    cnt += 1
-
-for i in range(cnt):
-    if arr[i] % 2 == 0:
-        sum_val += arr[i]
-        cnt_2 += 1
-
+    if elem % 2 == 0:
+        sum_val += elem
+        cnt += 1
+    
 print(cnt_2, sum_val)
