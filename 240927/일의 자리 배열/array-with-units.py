ce01090ba45b7369e491aa4bc7dfa7bb495b1912
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-27 16:19:19 +0900

    Title: 일의 자리 배열, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240927/일의 자리 배열/array-with-units.py
+++ 240927/일의 자리 배열/array-with-units.py
@@ -12,3 +12,12 @@ for i in range(2, 10):
 
 for elem in arr:
     print(elem, end=' ')
+
+"""
+p1, p2 = tuple(map(int, input().split()))
+arr = []
+arr.append(p1)
+arr.append(p2)
+for i in range(2, 10):
+    arr.append((arr[i-2]+arr[i-1])%10)
+"""
