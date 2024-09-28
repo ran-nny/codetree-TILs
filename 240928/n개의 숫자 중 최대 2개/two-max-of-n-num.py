7a7e019d30ce384913074454b0a196dad0b85ee9
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-28 16:43:20 +0900

    Title: n개의 숫자 중 최대 2개, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240928/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+arr_false = []
+
+max_value = arr[0]
+for elem in arr:
+    if elem > max_value:
+        max_value = elem
+
+arr_2 = [elem for elem in arr if elem != max_value]
+
+max_2_value = arr_2[0]
+for elem in arr_2:
+    if elem > max_2_value:
+        max_2_value = elem
+
+print(max_value, max_2_value)
+# 제거된 값 추출
+# removed_values = [elem for elem in n_arr if elem == value_to_remove]
+
+# 리스트에서 특정 값 제거
+# n_arr = [elem for elem in n_arr if elem != value_to_remove]
+
+# print("제거된 값들:", removed_values)  # 출력: [2, 2]
+# print("남은 리스트:", n_arr)  # 출력: [1, 3, 4, 5]
