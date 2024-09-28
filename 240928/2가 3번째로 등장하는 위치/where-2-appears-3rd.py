c5e2b785f7268d4657c08f3364ee918079e1743c
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-28 14:04:58 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240928/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240928/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -1,3 +1,4 @@
+n = int(input())
 n_arr = list(map(int, input().split()))
 
 idx = []
@@ -5,6 +6,6 @@ for i, elem in enumerate(n_arr):
     if elem == 2:
         idx.append(i)  # 인덱스를 추가
 
-# idx의 길이를 체크한 후 접근
-if len(idx) >= 3:  # idx에 3개 이상의 요소가 있는지 확인
-    print(n_arr[idx[2]])  # 세 번째 인덱스 출력
+real_idx = idx[2]
+
+print(real_idx+1)
