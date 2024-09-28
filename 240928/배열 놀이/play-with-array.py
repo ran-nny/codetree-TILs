fa32bdca376c97753c0f4551170f0d7148951460
란님 <113058078+ran-nny@users.noreply.github.com>
2024-09-28 12:28:49 +0900

    Title: 배열 놀이, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240928/배열 놀이/play-with-array.py
+++ 240928/배열 놀이/play-with-array.py
@@ -1,27 +1,25 @@
 n, q = tuple(map(int, input().split()))
 
-# n개의 원소를 담은 리스트
-arr_n = list(map(int, input().split()))
+# n개의 원소를 담은 튜플
+arr_n = tuple(map(int, input().split()))
 
 idx = -1
-for i in range(1, q+1): # q개의 질의
-    if i % 3 == 1: # 1 a
-        one, a = tuple(map(int, input().split())) # 1, a
-        print(arr_n[a-1])
+# 하나의 쿼리 진행
+for _ in range(1, q+1):
+    # 어떤 쿼리를 받을 것인지 arr_input의 첫번째 인자로 결정
+    arr_input= tuple(map(int, input().split()))
 
-    elif i % 3 == 2: # 2 b
-        two, b = tuple(map(int, input().split())) # 2 b
-        for j in range(1, n+1): # 몇 번째?
-            if arr_n[j-1] == b:
-                print(j)
+    if arr_input[0] == 1: # 1 a
+        print(arr_input[1])
+
+    elif arr_input[0] == 2: # 2 b
+        for i in range(1, n+1):
+            if arr_n[i-1] == arr_input[1]: # b랑 같은지,,?
+                print(i)
                 idx += 1
-                break
-        
-        # 원소 전부 확인했는데도 없으면? 
         if idx == -1:
             print(0)
 
-    else: # 3 s e
-        three, s, e = tuple(map(int, input().split()))
-        for k in range(s, e+1):
-            print(arr_n[k-1], end=' ')
+    else:
+        for i in range(arr_input[1], arr_input[2]+1):
+            print(arr_n[i-1], end=' ')
