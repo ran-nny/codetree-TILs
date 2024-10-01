4303b5b7d11db4079bb079f9599c82367db2c903
란님 <113058078+ran-nny@users.noreply.github.com>
2024-10-01 21:58:19 +0900

    Title: 특정 원소들의 합, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 241001/특정 원소들의 합/sum-of-specific-elements.py
+++ 241001/특정 원소들의 합/sum-of-specific-elements.py
@@ -9,13 +9,13 @@ for i in range(4):
     if i == 0:
         sum_val += arr_2d[0][0]
     if i == 1:
-        for j in range(1):
+        for j in range(2):
             sum_val += arr_2d[1][j]
     if i == 2:
-        for j in range(2):
+        for j in range(3):
             sum_val += arr_2d[2][j]
     if i == 3:
-        for j in range(3):
+        for j in range(4):
             sum_val += arr_2d[3][j]
 
 print(sum_val)
