a5253a2865865b0bc5ac34b656bba88f31c37621
란님 <113058078+ran-nny@users.noreply.github.com>
2024-10-01 22:18:49 +0900

    Title: 숫자 직사각형, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 241001/숫자 직사각형/rectangle-of-numbers.py
+++ 241001/숫자 직사각형/rectangle-of-numbers.py
@@ -1,4 +1,5 @@
 # 3X5행렬
+# n은 행, m 은 열 : m 이 안에 있는 리스트
 n, m = list(map(int, input().split()))
 arr_2d = [
     [0 for _ in range(m)]
@@ -6,8 +7,8 @@ arr_2d = [
 ]
 
 num = 1
-for i in range(m):
-    for j in range(n):
+for i in range(n):
+    for j in range(m):
         arr_2d[i][j] = num
         num += 1
 
