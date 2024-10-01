e6b77a1dce205e1539778d2d4791c660c6d0f604
란님 <113058078+ran-nny@users.noreply.github.com>
2024-10-01 21:17:14 +0900

    Title: 배열의 평균, Time: 112ms, Memory: 29MB, Status: Passed - CodeTree

--- 241001/배열의 평균/ave-of-array.py
+++ 241001/배열의 평균/ave-of-array.py
@@ -1,6 +1,6 @@
 arr_2d = [
     list(map(int, input().split()))
-    for _ in range(4)
+    for _ in range(2)
 ]
 
 avg_1 = 0
@@ -15,12 +15,13 @@ sum_val = 0
 for i in range(4):
     for j in range(2):
         sum_val += arr_2d[j][i]
-        avg_2 = f"{sum_val / 2.1f}"
-        print(avg_2, end=' ')
+    avg_2 = f"{sum_val / 2:.1f}"
+    print(avg_2, end=' ')
+    sum_val = 0
     
 print()
 
 whole_sum = 0 
 for i in range(2):
     whole_sum += sum(arr_2d[i])
-print(f"{whole_sum/8:1f}")
+print(f"{whole_sum/8:.1f}")
