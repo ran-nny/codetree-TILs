c037653bb470778944cb7a5549a3212b48747c26
란님 <113058078+ran-nny@users.noreply.github.com>
2024-10-01 21:55:33 +0900

    Title: 특정 원소들의 합, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241001/특정 원소들의 합/sum-of-specific-elements.py
@@ -0,0 +1,21 @@
+# 4X4 배열
+arr_2d = [
+    list(map(int, input().split()))
+    for _ in range(4)
+]
+
+sum_val = 0
+for i in range(4):
+    if i == 0:
+        sum_val += arr_2d[0][0]
+    if i == 1:
+        for j in range(1):
+            sum_val += arr_2d[1][j]
+    if i == 2:
+        for j in range(2):
+            sum_val += arr_2d[2][j]
+    if i == 3:
+        for j in range(3):
+            sum_val += arr_2d[3][j]
+
+print(sum_val)
