1a309654f5338a710162c55b29dd59fc65915479
ran-nny <113058078+ran-nny@users.noreply.github.com>
2024-10-04 02:25:49 +0900

    Title: 격자에 점 그리기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241004/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,17 @@
+n, m = tuple(map(int, input().split()))
+
+placed = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+num = 1
+for i in range(m):
+    r, c = tuple(map(int, input().split()))
+    placed[r-1][c-1] = num
+    num += 1
+
+for row in placed:
+    for elem in row:
+        print(elem, end=' ')
+    print()
