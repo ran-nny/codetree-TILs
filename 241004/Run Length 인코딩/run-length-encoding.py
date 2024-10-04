eb289705c8c62fb9a45b7760e9130b6b0b8d0d81
ran-nny <113058078+ran-nny@users.noreply.github.com>
2024-10-04 17:05:21 +0900

    Title: Run Length 인코딩, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 241004/Run Length 인코딩/run-length-encoding.py
+++ 241004/Run Length 인코딩/run-length-encoding.py
@@ -1,17 +1,15 @@
 a = input()
-encoding = []
 # 연속해서 나온 문자와 연속해서 나온 개수로 나타내는 방식
-encoding.append(a[0])
+strings = a[0]
 cnt = 1
 for i in range(1, len(a)):
     if a[i-1] == a[i]:
         cnt += 1
     if a[i-1] != a[i]:
-        encoding.append(cnt)
-        encoding.append(a[i])
+        strings = strings+str(cnt)+a[i]
         cnt = 1
-encoding.append(cnt)
+strings = strings+str(cnt)
 
-print(len(encoding))
-for elem in encoding:
+print(len(strings))
+for elem in strings:
     print(elem, end='')
