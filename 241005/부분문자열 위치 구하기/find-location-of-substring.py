5c9a6a1c6fb60b0d07bd7497346e6cd9893ef6d5
ran-nny <113058078+ran-nny@users.noreply.github.com>
2024-10-05 17:18:53 +0900

    Title: 부분문자열 위치 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241005/부분문자열 위치 구하기/find-location-of-substring.py
+++ 241005/부분문자열 위치 구하기/find-location-of-substring.py
@@ -1,37 +1,27 @@
-string = input()
-target = input()
+import sys
 
-# print(string.find(target))
-target_len = len(target)
-string_len = len(string)
+input_str = input()
+target_str = input()
 
-# 부분 문자열의 시작 인덱스가 될 수 있는 목록
-start_idx = []
-idx_cnt = 0
+input_len, target_len = len(input_str), len(target_len)
 
-# 같을 수 있는 가능성이 있는 인덱스 찾기
-for i in range(string_len):
-    if string[i] == target[0]:
-        start_idx.append(i)
-        idx_cnt += 1
+# 입력 문자열의 각 문자를 시작점으로 하여 목적 문자열을 만들 수 있는지 확인
+for i in range(input_len):
+    # input_str의 i부터 i+target_len -1까지의 원소가 (앞의 i인덱스 포함된거니까 -1해줘야 길이 맞음)
+    # target_len의 0부터 target_len-1까지의 원소와 정확히 일치하는지를 보면 된다. 
 
-# 하나라도 가능성 있는 인덱스가 있을 때
-if idx_cnt != 0:
+    # 만약 Input_str의 끝 원소인 i + target_len -1 번째가 존재하지 않으면 비교하지 않는다. 
+    if i + target_len -1 >= input_len:
+        continue
 
-    cnt = 0
-    for i in start_idx:
-        for j in range(target_len-1):
-            if string[i+1] == target[j+1]:
-                i += 1
-                cnt += 1
-            else:
-                break
-    # 인덱스가 모두 맞으면 바로 아웃! 
-        if cnt == target_len -1:
-            print(i-1)
-            break
-        cnt = 0
+    # input_str의 s_idx에서 e_idx까지의 문자열과
+    # target_str의 s_idx에서 e_idx까지의 문자열과 일치하는지를 비교
+    is_matched = True
+    for j in range(target_len):
+        if input_str[i+j] != target_str[j]: # 어차피 i는 픽스 - i+j로 표현하는 방법도!
+            is_matched = False
+        if is_matched:
+            print(i)
+            sys.exit(0)
 
-# 하나라도 가능성 있는 인덱스가 없을 때
-else:
-    print(-1)
+print(-1)
