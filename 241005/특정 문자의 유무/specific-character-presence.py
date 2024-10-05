9e1bcef13341bd036903538cad2011420b381c80
ran-nny <113058078+ran-nny@users.noreply.github.com>
2024-10-05 13:07:16 +0900

    Title: 특정 문자의 유무, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241005/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,14 @@
+string = input()
+
+s1 = 'ee'
+s2 = 'ab'
+
+if s1 in string:
+    print('Yes', end=' ')
+else:
+    print('No', end=' ')
+
+if s2 in string:
+    print('Yes', end=' ')
+else:
+    print('No', end=' ')
